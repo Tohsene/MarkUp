1c796457a4e9814b2202fb87dad6f67ddbfdd06e
support <support@DESKTOP-GOA92KN>
2021-11-10 21:11:25 +0100

    Add project files.

---
+++ correction/MarkUpgrader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace correction
+{
+    class MarkUpgrader
+    {
+       public int Upgrade(int oldMark)
+        {
+            /*int newMark = 0;
+            
+            if (oldMark>50)
+            {
+                newMark = oldMark + 5;
+
+            }
+            else
+            {
+                newMark = oldMark + 10
+            }
+            */
+            int newMark = oldMark + 5;
+            if (oldMark < 50)
+                newMark = oldMark + 10;
+
+            return newMark;
+        }
+    }
+}
---
+++ correction/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace correction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Directory.CreateDirectory("C:\\Principals Record");
+                var path = "C:\\Principals Record\\newscore.txt";
+                var oldPath = "C:\\Principals Record\\oldScore.txt";
+                //File.Create(path);
+                File.WriteAllText(path, "");
+
+                foreach (var line in File.ReadAllLines(path))
+                {
+                    var dMark = line.Split('-')[1];
+
+                    var markUp = new MarkUpgrader();
+                    var newMark = markUp.Upgrade(Int32.Parse(dMark));
+                    var newLineContent = line.Split("-")[0] + "-" + newMark;
+                    File.AppendAllLines(path, new string[] { newLineContent });
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
